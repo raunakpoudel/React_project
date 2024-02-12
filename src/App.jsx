c0f1c339d068d81c9a39832fa75fd76d445dd7df
raunakpoudel <raunakpoudel8@gmail.com>
2024-02-12 23:06:48 +0545

    some new file created and some changes were made in file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Nav from './component/Nav'
-import Hero from './component/Hero'
+import MidComponent from './component/MidComponent'
+import Button from './component/Button'
 const App = () => {
   return (
     <>
     <Nav/>
-    <Hero/>
+    <MidComponent/>
+    {/* <Button 
+       name="Click Me!"
+    /> */}
     
     
     
---
+++ src/component/MidComponent.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Hero.css'
+import Button from './Button'
+const MidComponent = () => {
+  return (
+    
+    <> <div className="box">
+
+
+<div className="left">
+<div className="desc">
+<h3>Hi,I am</h3> 
+<h1>Jeevan Poudel</h1>
+
+</div>
+<div id="button">
+
+<Button id='button'
+name='Explore Me'
+/>
+</div>
+
+</div>
+
+<div className="right">
+ <img src="https://as1.ftcdn.net/v2/jpg/05/88/95/34/1000_F_588953411_vnwqBXqcoGBaR9zTQKcq37FtjAEax50k.webp" alt="" />
+</div>
+</div>
+</>
+  )
+}
+
+export default MidComponent
